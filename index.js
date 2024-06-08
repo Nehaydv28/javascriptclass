5ada0f1e0258177d79e68d5f07a2fd8bedcbad71
Nehaydv28 <mamtayadav4569832@gmail.com>
2024-06-08 11:48:17 +0530

    basic js

---
+++ index.js
@@ -0,0 +1,76 @@
+// // // // // //var
+// // // // // var num = 10;
+// // // // // {
+// // // // //     num = 20;
+// // // // // }
+// // // // // console.log("var",num)
+
+// // // // // //let
+// // // // // let num1 =10
+
+// // // // // {
+// // // // //     num1 =20
+// // // // // }
+// // // // // comsole.log("let",num1)
+
+// // // // // //const
+// // // // // const num2 ={"name":"raj"}
+
+// // // // // {
+// // // // //     num2="hello"
+// // // // // }
+// // // // // console.log("const",num2)
+
+// // // // let num1 = 10;
+// // // // let num2 = 20;
+
+// // // // let sum = num1+num2;
+// // // // console.log(sum)
+
+// // // // let sub = num1-num2;
+// // // // console.log(sub)
+
+// // // // let div = num1/num2;
+// // // // console.log(div)
+
+// // // // let mul = num1*num2;
+// // // // console.log(mul)
+
+// // // let num1 =5;
+// // // let num2 =10;
+
+// // // // let sum = num++ + num + num++
+// // // // console.log(sum)
+
+// // // let sum = --num1 + ++num2 + num1-- + num2++
+// // // console.log(sum)
+// // // console.log(num1)
+// // // console.log(num2)
+
+// // // let checkIn = true
+// // // let checkOut = true;
+
+// // // console.log(checkIn == checkOut);
+
+// // // let checkIn = 10
+// // // let checkOut = "10";
+
+// // // console.log(checkIn == checkOut);
+
+// // let checkIn = 10
+// // let checkOut = "10";
+
+// // console.log(checkIn === checkOut);
+// // console.log(typeof checkIn)
+// // console.log(typeof checkOut)
+
+// // let a = 10
+// // let b = 20;
+// // let c = 30
+// // let result = ( a < b && a < c ) ? "a": ( b < c ) ? "b": "c";
+// // console.log(result);
+
+// let age = 20;
+// let result =( age > 18) ? "cast vote": "not";
+// console.log(result);
+
