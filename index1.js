fa062a29b8543717417ab24241c96f261fcea6a0
Nehaydv28 <mamtayadav4569832@gmail.com>
2024-06-10 11:30:44 +0530

    func obj str arr

---
+++ index1.js
@@ -0,0 +1,40 @@
+// function add(){
+//     console.log("hello")
+// }
+// add();
+
+
+// function add(...a){ //para
+//     console.log(a)
+// }
+// add(12,34) ;//arg
+
+
+// const result = function (a=10){
+//     return a+a;
+// }
+// console.log(result());
+
+
+// const result = function (a){
+//     return a+a;
+// }
+// console.log(result(20));
+
+
+// const result = (a) => {
+//     return a+a;
+// }
+// console.log(result(20));
+
+
+// ;
+// (()=>{
+//     console.log("hello");
+// })()
+
+
+const obj = {"key":"val","first name":"niaa"}
+
+console.log(obj["key"])
+console.log(obj["first name"]);
